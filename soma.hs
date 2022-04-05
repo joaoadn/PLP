baace6d0350ecabfa8a911bd477c83ecb4f57c06
Joao Dias <joaoadn@outlook.com>
2022-04-05 14:56:30 -0300

    Enter

--- soma.hs
+++ soma.hs
@@ -1,2 +1,2 @@
-soma :: Int -> Int -> Int
-soma x y  = x+y
+soma :: Int -> Int -> Int --funçao recebe dois valores inteiros e retorna um valor inteiro
+soma x y  = x+y --funcao é chamada para dois valores distintos a serem somados 
