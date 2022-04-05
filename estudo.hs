444c480d9d4019a37578683bd2ad593e89857fea
Joao Dias <joaoadn@outlook.com>
2022-04-05 15:35:05 -0300

    Enter

---
+++ estudo.hs
@@ -0,0 +1,7 @@
+potencia = 2 exp 9 --potencia
+logDoNumero = log 9 --log
+elevaA2 = 9 ** 2 --elevado a 2
+truncaONumero = truncate 9.999 --truncar (remove a parte fracionária)
+arredondaONumero = round 9.999 --arrendondar (normal)
+arredondaParaCima = ceiling 9.999 --arredondar (para cima)
+arredondaParaBaixo = floor 9.999 --arredondar (para baixo)
--- soma.hs
+++
@@ -1,2 +0,0 @@
-soma :: Int -> Int -> Int --funçao recebe dois valores inteiros e retorna um valor inteiro
-soma x y  = x+y --funcao é chamada para dois valores distintos a serem somados 
