f7befa432633df76c58c043f24c81a5e4056a5bd
Joao Dias <joaoadn@outlook.com>
2022-04-05 18:16:50 -0300

    Enter

--- estudo.hs
+++ estudo.hs
@@ -1,7 +1,7 @@
-potencia = 2 exp 9 --potencia
-logDoNumero = log 9 --log
-elevaA2 = 9 ** 2 --elevado a 2
-truncaONumero = truncate 9.999 --truncar (remove a parte fracionária)
-arredondaONumero = round 9.999 --arrendondar (normal)
+potencia =  exp 9  --potencia
+logdoNumero = log  --log
+elevadoadois = 9 ** 2 --elevado a 2
+truncaValor = truncate 9.999 --truncar (remove a parte fracionária)
+arredondaValor = round 9.999 --arrendondar (normal)
 arredondaParaCima = ceiling 9.999 --arredondar (para cima)
 arredondaParaBaixo = floor 9.999 --arredondar (para baixo)
